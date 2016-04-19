1c148948eb9e568d767d84de46e42981d59c4721
Yi Shang <SHANG@dhcp0324.nic.resnet.group.upenn.edu>
2016-04-18 21:55:37 -0400

    add GameView.java, to show the lobby display of the game

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * This is the main tester class.
+ * It creates one Game View and simulates the GUI game play.
+ * 
+ * @author SHANG
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		GameView gv = new GameView();
+
+	}
+
+}
