56e4a20a826e85b39194be3273166598b7a53224
Yi Shang <SHANG@hntvlan569.1345.wlan.wireless-resnet.upenn.edu>
2016-04-24 15:18:46 -0400

    add distance() method in FourDirectionMonster.java

--- src/model/FourDirectionMonster.java
+++ src/model/FourDirectionMonster.java
@@ -37,7 +37,7 @@ public class FourDirectionMonster extends Monster {
 
 			@Override
 			public int compare(Site s1, Site s2) {
-				return s1.manhattanTo(rogue) - s2.manhattanTo(rogue);
+				return distance(s1, rogue) - distance(s2, rogue);
 			}
         	
 		});
@@ -50,6 +50,43 @@ public class FourDirectionMonster extends Monster {
         
         // return the site with minimum distance to rogue
         return pq.poll();
+	}
+	
+	/**
+	 * gets the distance between start and end
+	 * @param start start site
+	 * @param end end site
+	 * @return distance between them
+	 */
+	private int distance(Site start, Site end) {
+		// return 0 if same site
+		if (start.equals(end)) return 0;
+		
+        // get 4 adjacent sites
+        int x = start.getX();
+        int y = start.getY();
+        
+        Site east = new Site(x + 1, y);
+        Site west = new Site(x - 1, y);
+        Site north = new Site(x, y - 1);
+        Site south = new Site(x, y + 1);
+        
+        // get 4 distances to end
+        int eastDist = Integer.MAX_VALUE;
+        int westDist = Integer.MAX_VALUE;
+        int northDist = Integer.MAX_VALUE;
+        int southDist = Integer.MAX_VALUE;
+        
+        if (dungeon.isLegalMove(start, east)) eastDist = distance(east, end);
+        if (dungeon.isLegalMove(start, west)) westDist = distance(west, end);
+        if (dungeon.isLegalMove(start, north)) northDist = distance(north, end);
+        if (dungeon.isLegalMove(start, south)) southDist = distance(south, end);
+        
+        // return the smallest distance
+        if (eastDist < westDist && eastDist < northDist && eastDist < southDist) return eastDist + 1;
+        if (westDist < eastDist && westDist < northDist && westDist < southDist) return westDist + 1;
+        if (northDist < eastDist && northDist < westDist && northDist < southDist) return northDist + 1;
+        return southDist + 1;
 	}
 
 }
